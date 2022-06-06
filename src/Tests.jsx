26c73e7c9bbac8a9109f096b07f554a8913e98ba
SebasProgrammer2020 <sebastian.londono@atlanticsoft.us>
2022-06-06 08:59:35 -0500

    actualización ajustes de colores, y estilos, se añadio la funcionalidad de mostrar el rastro, se cambio funcionalidad de mostrar el bloque al finalizar

--- src/Tests.jsx
+++ src/Tests.jsx
@@ -8,11 +8,51 @@ const Tests = () => {
   const [positionTop, setPositionTop] = useState(0);
   const [positionLeft, setPositionLeft] = useState(0);
   const [startTest, setStartTest] = useState();
+  const [blockRoute, setBlockRoute] = useState([
+    { top: "134px", left: "14px" },
+  ]);
+  const [showTrace, setShowTrace] = useState();
   let modifier = 38;
   const block = document.getElementById("block");
 
-  let goalPositions = { top: 362, left: 242 };
-  let startPostions = { top: 134, left: 14 };
+  let test = {
+    goalPosition: { top: 362, left: 242 },
+    startPostion: { top: 134, left: 14 },
+    obstacles: [
+      { top: 286, left: 14 },
+      { top: 248, left: 128 },
+      { top: 324, left: 166 },
+      { top: 286, left: 280 },
+    ],
+  };
+
+  const { goalPosition, startPostion, obstacles } = test;
+
+  const handleBlockedByBug = (nextTop, nextLeft) => {
+    let availableToMove = true;
+    // console.log(nextTop, nextLeft);
+    obstacles.map((obstacle) => {
+      if (`${obstacle.top}px` === nextTop) {
+        console.log(nextTop, nextLeft);
+        console.log(`${obstacle.top}px`);
+        console.log(nextTop);
+
+        // document.getElementById("block").style.top = `${
+        //   parseInt(nextTop) - modifier
+        // }px`;
+
+        // availableToMove = false;
+      }
+      // else if (`${obstacle.left}px` === nextLeft) {
+      //   document.getElementById("block").style.left = `${
+      //     parseInt(nextLeft) + modifier
+      //   }px`;
+      // } else {
+      //   return;
+      // }
+    });
+    // return availableToMove;
+  };
 
   const handleDirectionClick = (e) => {
     let instructionsList = document.querySelector(".ordersView");
@@ -56,6 +96,9 @@ const Tests = () => {
       }
       setPositionTop(style.top);
       setPositionLeft(style.left);
+      handleBlockedByBug(style.top, style.left);
+      setBlockRoute(blockRoute.concat({ top: style.top, left: style.left }));
+      // console.log(style.top, style.left, "actual");
     } else {
       return;
     }
@@ -76,12 +119,12 @@ const Tests = () => {
     document.getElementById("goal").style.display = "flex";
     handleDirectionClick(e);
   };
-  console.log(positionTop, "top");
-  console.log(positionLeft, "left");
 
   const revealBlock = () => {
-    document.getElementById("block").style.display = "flex";
-    document.getElementById("goal").style.display = "none";
+    let block = document.getElementById("block");
+    block.style.display = "flex";
+    block.style.zIndex = 10;
+    setShowTrace(true);
   };
 
   const validateSequence = () => {
@@ -115,8 +158,8 @@ const Tests = () => {
           height: "23px",
           backgroundColor: "yellow",
           position: "absolute",
-          top: startPostions.top,
-          left: startPostions.left,
+          top: startPostion.top,
+          left: startPostion.left,
         }}
       ></div>
       <div
@@ -127,10 +170,26 @@ const Tests = () => {
           height: "23px",
           backgroundColor: "mediumaquamarine",
           position: "absolute",
-          top: 134,
-          left: 14,
+          top: "134px",
+          left: "14px",
         }}
       ></div>
+      {showTrace &&
+        blockRoute.map((coodinates, index) => (
+          <div
+            key={index}
+            id="block"
+            style={{
+              display: showTrace ? "flex" : "none",
+              width: "23px",
+              height: "23px",
+              backgroundColor: "mediumaquamarine",
+              position: "absolute",
+              top: coodinates.top,
+              left: coodinates.left,
+            }}
+          ></div>
+        ))}
       <div
         id="goal"
         style={{
@@ -139,29 +198,78 @@ const Tests = () => {
           height: "23px",
           backgroundColor: "red",
           position: "absolute",
-          top: goalPositions.top,
-          left: goalPositions.left,
+          top: goalPosition.top,
+          left: goalPosition.left,
         }}
       ></div>
+      {/* {obstacles.map((obstacle, index) => (
+        <div
+          key={index}
+          id="obstacle"
+          style={{
+            display: "flex",
+
+            width: "23px",
+            height: "23px",
+            position: "absolute",
+            top: obstacle.top,
+            left: obstacle.left,
+          }}
+        >
+          <img
+            src="https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/undefined/external-bug-coding-kiranshastry-lineal-color-kiranshastry.png"
+            alt="bug"
+          />
+        </div>
+      ))} */}
+
       <button onClick={handleStartTest}>iniciar test</button>
       <button onClick={validateSequence}>Enviar respuestas</button>
       <div className="test">
         <div className="testView">prueba 1</div>
         <div className="controls">
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <button onClick={handleDirectionClick} name="arriba">
-              arriba
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={handleDirectionClick} className="buttonStyle">
+              <img
+                name="arriba"
+                className="imgSize"
+                src="https://img.icons8.com/stickers/100/000000/thick-arrow-pointing-up.png"
+                alt="up icon"
+              />
             </button>
           </div>
           <div style={{ display: "flex", gap: 7, marginTop: 7 }}>
-            <button onClick={handleDirectionClick} name="izquierda">
-              izquierda
+            <button className="buttonLeftStyles" onClick={handleDirectionClick}>
+              <img
+                name="izquierda"
+                className="imgSize"
+                alt="left icon button"
+                src="https://img.icons8.com/stickers/100/undefined/arrow-pointing-left.png"
+              />
             </button>
-            <button onClick={handleDirectionClick} name="abajo">
-              abajo
+            <button className="buttonStyle" onClick={handleDirectionClick}>
+              <img
+                name="abajo"
+                className="imgSize"
+                src="https://img.icons8.com/stickers/100/000000/thick-arrow-pointing-down.png"
+                alt="down icon"
+              />
             </button>
-            <button onClick={handleDirectionClick} name="derecha">
-              derecha
+            <button
+              className="buttonRightStyles"
+              onClick={handleDirectionClick}
+            >
+              <img
+                name="derecha"
+                className="imgSize"
+                alt="right icon button"
+                src="https://img.icons8.com/stickers/100/undefined/right.png"
+              />
             </button>
           </div>
           <div className="ordersView"></div>
@@ -170,6 +278,53 @@ const Tests = () => {
       <button style={{ marginLeft: 60 }}>
         <Link to="/report">Ver reporte</Link>
       </button>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://icons8.com/icon/o3EJ3VrJkeY4/gruesa-flecha-apuntando-hacia-arriba"
+            rel="noreferrer"
+          >
+            Gruesa flecha apuntando hacia arriba icon by Icons8
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://icons8.com/icon/eXJY0kFTvF4f/gruesa-flecha-apuntando-hacia-abajo"
+            rel="noreferrer"
+          >
+            Gruesa flecha apuntando hacia abajo icon by Icons8
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://icons8.com/icon/uauxHDCVDyl8/bug"
+          >
+            Bug icon by Icons8
+          </a>
+        </li>
+        <li>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://icons8.com/icon/SSQknWWyQFW8/flecha-apuntando-hacia-la-izquierda"
+          >
+            Flecha apuntando hacia la izquierda icon by Icons8
+          </a>
+        </li>
+        <li>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://icons8.com/icon/lx6QKDKp28mI/derecha"
+          >
+            Derecha icon by Icons8
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
